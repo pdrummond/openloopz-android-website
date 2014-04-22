ac5d526e21142a098002a7467f35d720e628c5fc
Paul Drummond <paul.drummond@iode.co.uk>
2014-04-22 19:59:24 +0100

    Initial commit

---
+++ build/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+    
+        <meta charset="utf-8" />
+        <!--[if IE]>
+        <meta http-equiv="imagetoolbar" content="no" />
+        <![endif]-->
+        
+        
+		<title>OpenLoopz: Location-aware task management</title>
+        
+        <!-- CSS -->
+
+		<link href="stylesheets/screen.css" media="all" rel="stylesheet" type="text/css" />
+		
+				        
+        <!--[if IE]>
+        <style type="text/css">
+        	@font-face { font-family: LatoReg; src: url(fonts/lato-fontfacekit/Lato-Reg-webfont.eot); }
+            @font-face { font-family: LatoLight; src: url(fonts/lato-fontfacekit/Lato-Lig-webfont.eot); }
+            @font-face { font-family: LatoBold; src: url(fonts/lato-fontfacekit/Lato-Bol-webfont.eot); }
+        </style>
+        <![endif]-->
+        
+        <!-- JAVASCRIPT -->
+        
+        <!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    	<![endif]-->
+        
+        <!-- SEO -->
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+
+		<!-- ICONS -->
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="apple-touch-icon" href="favicon-ios-57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="favicon-ios-72.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="favicon-ios-114.png" />
+        
+        
+        <!-- SHARETHIS SCRIPTS -->
+        <script type="text/javascript">var switchTo5x=true;</script>
+		<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+        <script type="text/javascript">stLight.options({publisher: "ur-577245b1-fc1d-a159-6a11-8d9941396231"}); </script>
+
+    
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32227013-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+    </head>
+    
+    <body>
+    
+        
+    	<div id="pageWrapper">
+        
+        	<div id="header">
+        
+                <div class="headerLeft">
+                    
+                    <h3><a href="https://play.google.com/store/apps/details?id=iode.openloopz">free app</a></h3>
+                    <h4><a href="http://openloopz.tumblr.com/">Blog</a></h4>
+                    <h4><a href="https://groups.google.com/forum/?fromgroups#!forum/openloopz">Forum</a></h4>
+                    
+                    <h1>openloopz</h1>
+                    
+                    <h2>Location-aware<br> task management</h2>
+                    
+                    <p><a href="https://play.google.com/store/apps/details?id=iode.openloopz"><img src="images/get_it_on_play_logo_large.png" alt="Get it on Google Play"></a></p>
+                    
+                    
+    <!--                <div class="share">
+                        
+                        <span class='st_facebook_vcount'></span>
+                        <span class='st_twitter_vcount'></span>
+                        <span class='st_googleplus_vcount'></span>
+                        <span class='st_email_vcount'></span>
+                        
+                    </div>
+        -->            
+                </div>
+                
+                <div class="headerRight">
+                    <img src="images/deviceMock.jpg" alt="openloopz UI">
+                </div>
+        	
+        	</div>
+            
+            
+            
+            <div id="features">
+            	
+                <div class="featuresContent">
+                
+                	<h2>Features</h2>
+                	
+                    <div class="featureItem">
+                    	<h4>Now List</h4>
+                        <p>Avoid managing a long list of tasks and focus on what you need to be doing now based on your current location</p>
+                    </div>
+                    
+                    <div class="featureItem">
+                    	<h4>Contexts</h4>
+                        <p>Focus on the context in which you can get your tasks done such as <i>At Work</i> or <i>During Lunch</i> and let OpenLoopz take care of the rest</p>
+                    </div>
+                    
+                    <div class="featureItem">
+                    	<h4>Inbox</h4>
+                        <p>When in a rush, create tasks quickly using the Inbox feature. Simply enter the name of the action without specifying any context or deadline and it will be stored in the Inbox until you come to organise it when you have more time
+                    </div>
+                    
+                    <div class="featureItem">
+                    	<h4>Review</h4>
+                        <p>When life gets in the way and you can&rsquo;t find the time to complete your now actions, don&rsquo;t worry. You can check out the Review List at any time to see what you have missed and why</p>
+                    </div>
+                    
+                    <div class="featureItem">
+                    	<h4>Projects</h4>
+                        <p>Simple but effective project management features provides grouping of actions with a focus on <i>Next Actions</i></p>
+                    </div>
+                    
+                    <div class="featureItem">
+                    	<h4>Next Actions</h4>
+                        <p>A summary list of your overall workload, your Next Action list shows the next action for each project. By keeping on top of your next actions you will make progress in each project equally, increasing your productivity</p>
+                    </div>
+                    
+                    
+                </div>
+            
+            </div>
+            
+            
+            
+            
+            
+            <div id="quotes">
+            	
+                <h2>What users are saying...</h2>
+                
+                <p>&ldquo;When I arrive at work in the morning, it&rsquo;s so easy to glance at my Now List to see my work load for the day!&rdquo;</p>
+                <p>&ldquo;It&rsquo;s so helpful to be reminded of all the websites I want to checkout during my lunch break&rdquo;</p>
+                <p>&ldquo;Being reminded of my shopping list as I drive near to the local store is so useful!&rdquo;</p>
+                <p>&ldquo;Before OpenLoopz, I would focus on one project at a time. Now with Next Actions I find myself finishing projects early without even realising it!&rdquo;</p>
+            
+            </div>           
+            
+            
+        </div>
+        
+        
+        
+        <footer id="footerWrapper">
+        	<div id="footerContent">
+            
+            	<div class="footerLeft">
+                	<p>&copy; 2012 Iode Software</p>
+                    <p>Contact: <a href="mailto:enquiries@iode.co.uk">enquiries@iode.co.uk</a></p>
+                </div>
+                
+                <div class="footerRight">
+                	<p><a href="http://iode.co.uk"><img src="images/footer-iode-logo.png" alt="a project by Iode Software"></a></p>
+                </div>
+            
+            </div>            
+        </footer>
+    
+
+
+
+	<!-- GOOGLE ANALYTICS TAGS -->
+
+
+	</body>
+
+</html>
